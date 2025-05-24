d920cf6eb9c1bafae0b5fef1e450f0d8da464527
ruchirmathur <ruchirmat@gmail.com>
2025-05-24 09:09:41 -0700

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -261,7 +261,7 @@ export const Dashboard: React.FC = () => {
     setTableError(null);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_HOST}/api/ask`, {
+      const response = await fetch(`healthoneapi-bqbrb8e7bvb4gxeq.eastus2-01.azurewebsites.net/api/ask`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ user_input: chatInput }),
