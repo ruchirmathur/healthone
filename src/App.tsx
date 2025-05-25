00d9ea5f44430145a59e0bbc11cd0b131560f301
ruchirmathur <ruchirmat@gmail.com>
2025-05-25 05:03:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Routes, Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import { AutoLogin } from './pages/AutoLogin';
